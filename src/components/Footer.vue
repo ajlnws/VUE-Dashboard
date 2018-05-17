2811c86149f71f5558535b91758b39f6b5861a31
Adrià Julià Newshore <adria.julia@newshore.es>
2018-05-17 14:17:56 +0200

    VUE-Dashboard

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="app-footer">
+    <span>&copy; 2018</span>
+    <span class="ml-auto">Powered by <a href="http://newshore.es">newshore_</a></span>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'c-footer'
+}
+</script>
